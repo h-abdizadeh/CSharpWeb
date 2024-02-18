ba96f50b591a18ba5debd7a2db5092d276ddef80
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-02-18 15:33:13 +0330

    Add files via upload

---
+++ AspNetCore7/SheypoorChi/SheypoorChi.DataLayer/Context/DatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SheypoorChi.DataLayer.Models;
+
+namespace SheypoorChi.DataLayer.Context;
+
+public class DatabaseContext:DbContext
+{
+    public DbSet<Role> Roles { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // base.OnConfiguring(optionsBuilder);
+        options.UseSqlServer(@"Data Source=.\SQLEXPRESS; 
+                                Initial Catalog=SheypoorChiDb;
+                                Trusted_Connection=SSPI;
+                                  Encrypt=false;
+                                    TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+
+        var adminRole = new Role()
+        {
+            Id = Guid.NewGuid(),
+            RoleName = "admin",
+            RoleTitle = "مدیر",
+        };
+
+        modelBuilder.Entity<Role>().HasData(adminRole);
+    }
+}
