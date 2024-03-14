a0b79d406e91aef586d85d445627edc3485a4224
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-03-14 18:03:45 +0330

    Add files via upload

---
+++ AspNetCore7/SheypoorChi/SheypoorChi.Core/Classes/ImageClass.cs
@@ -0,0 +1,63 @@
+﻿
+namespace SheypoorChi.Core.Classes;
+
+public class ImageClass
+{
+    string groupPath = "wwwroot/images/groups/";
+    public async Task<string> CreateSvg(string svgCode, string fileName)
+    {
+        //string svgFileName = fileName + ".svg";
+        string svgFileName = $"{fileName}.svg";
+        string savePath = "wwwroot/Images/groups/";
+
+        if (!Directory.Exists(savePath))
+            Directory.CreateDirectory(savePath);
+
+        string fileAddress =
+            Path.Combine(savePath, svgFileName);
+
+        await File.WriteAllTextAsync(fileAddress, svgCode);
+
+        return svgFileName;
+    }
+
+    public async Task<string> GetSvgCode(string fileName)
+    {
+        string fileAddress = "wwwroot/images/groups/" + fileName;
+
+        if (File.Exists(fileAddress))
+            return await File.ReadAllTextAsync(fileAddress);
+
+        return string.Empty;
+    }
+
+    public async Task<string> EditSvg(string svgCode,
+                                        string filename,
+                                          string oldFilename)
+    {
+        string svgFilename = $"{filename}.svg";
+
+        if (oldFilename != svgFilename &&
+            File.Exists(groupPath + oldFilename))
+        {
+            File.Delete(groupPath + oldFilename);
+        }
+
+        string fileAddress =
+            Path.Combine(groupPath, svgFilename);
+
+        await File.WriteAllTextAsync(fileAddress, svgCode);
+
+        return svgFilename;
+
+    }
+
+
+    public void DeleteSvg(string fileName)
+    {
+        if (File.Exists(groupPath+fileName))
+        {
+            File.Delete(groupPath + fileName);
+        }
+    }
+}
--- AspNetCore7/SheypoorChi/SheypoorChi.DataLayer/Models/Group.cs
+++ AspNetCore7/SheypoorChi/SheypoorChi.DataLayer/Models/Group.cs
@@ -8,15 +8,17 @@ public class Group
     [Key]
     public int Id { get; set; }
 
+    [Display(Name ="نام گروه",Prompt ="نام گروه")]
     [Required(ErrorMessage ="درج نام گروه الزامیست")]
     [MaxLength(15,ErrorMessage ="نام گروه حداکثر 15 کاراکتر")]
     public string GroupName { get; set; }
 
+    [Display(Name = "آیکن", Prompt = "کد آیکن")]
     public string? Img { get; set; }
 
     [Display(Name = "وضعیت نمایش")]
     public bool NotShow { get; set; } = false;
 
     //Group -> Product : n
-    virtual public ICollection<Product> Products { get; set; }
+    virtual public ICollection<Product>? Products { get; set; }
 }
