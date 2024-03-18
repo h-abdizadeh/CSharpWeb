cbf3e74b127786c3defb56346e0a3e3e790a0579
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-03-18 17:17:24 +0330

    Add files via upload

---
+++ AspNetCore7/SheypoorChi/ShepoorChi/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SheypoorChi.Core.Interface;
+using SheypoorChi.DataLayer.Models;
+
+namespace ShepoorChi.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class ProductsController : Controller
+{
+    IAdmin _admin;
+
+    public ProductsController(IAdmin admin)
+    {
+        _admin = admin;
+    }
+
+    //[HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var products = await _admin.GetProducts();
+
+        return View(products);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        var groups = await _admin.GetGroups();
+        ViewBag.Groups = new SelectList(groups, "Id", "GroupName");
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            //add product
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        ViewBag.Groups = 
+            new SelectList(await _admin.GetGroups(), "Id", "GroupName");
+
+        return View(product);
+    }
+}
