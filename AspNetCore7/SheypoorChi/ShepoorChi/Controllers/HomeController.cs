458f897da52337f6d2ae3fb0c267b5e19ed5a770
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-04-07 16:10:49 +0330

    Add files via upload

--- AspNetCore7/SheypoorChi/ShepoorChi/Controllers/HomeController.cs
+++ AspNetCore7/SheypoorChi/ShepoorChi/Controllers/HomeController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SheypoorChi.Core.Interface;
 
 namespace ShepoorChi.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    IAdmin _admin;
+
+    public HomeController(IAdmin admin)
     {
+        _admin = admin;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        //get all groups
+        var groups = await _admin.GetGroups();
+        //filter groups (notShow false)
+        //ViewBag.Groups = groups.Where(g => g.NotShow == false);
+        //ViewBag.Groups = groups.Where(g => g.NotShow is false);
+        ViewBag.Groups = groups.Where(g => !g.NotShow);
+
         return View();
     }
 }
