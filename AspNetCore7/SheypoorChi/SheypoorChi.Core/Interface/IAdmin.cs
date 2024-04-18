f6b9dd7acf0b9cb52b2fd07bedb796e8153ff087
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-04-18 17:26:10 +0330

    Add files via upload

--- AspNetCore7/SheypoorChi/SheypoorChi.Core/Interface/IAdmin.cs
+++ AspNetCore7/SheypoorChi/SheypoorChi.Core/Interface/IAdmin.cs
@@ -1,9 +1,13 @@
 ﻿using SheypoorChi.DataLayer.Models;
+using Microsoft.AspNetCore.Http;
 namespace SheypoorChi.Core.Interface;
 
 public interface IAdmin:IDisposable
 {
     public Task<List<Product>> GetProducts();
+    public Task<List<Product>> GetProducts(bool notShow=false);
     public Task<List<Group>> GetGroups();
+
+    public Task<bool> AddProduct(Product product,IFormFile imgFile);
 
 }
--- AspNetCore7/SheypoorChi/SheypoorChi.Core/Service/AdminService.cs
+++ AspNetCore7/SheypoorChi/SheypoorChi.Core/Service/AdminService.cs
@@ -1,6 +1,8 @@
 ﻿
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using SheypoorChi.Core.Classes;
 using SheypoorChi.Core.Interface;
 using SheypoorChi.DataLayer.Context;
 using SheypoorChi.DataLayer.Models;
@@ -14,6 +16,27 @@ public class AdminService : IAdmin
     public AdminService(DatabaseContext context)
     {
         _context = context;
+    }
+
+    public async Task<bool> AddProduct(Product product, IFormFile imgFile)
+    {
+        try
+        {
+            //upload product image
+            product.Img = await new ImageClass().SaveProductImg(imgFile);
+
+            //save product data
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine($"===> add product error ===> {e}");
+            return false;
+        }
     }
 
     public void Dispose()
@@ -33,6 +56,14 @@ public class AdminService : IAdmin
     public async Task<List<Product>> GetProducts()
     {
         var products = await _context.Products.ToListAsync();
+        return products;
+    }
+
+    public async Task<List<Product>> GetProducts(bool notShow = false)
+    {
+        var products =
+            await _context.Products.Where(p => p.NotShow == notShow).ToListAsync();
+
         return products;
     }
 }
