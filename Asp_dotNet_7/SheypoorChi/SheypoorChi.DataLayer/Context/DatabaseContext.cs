0ce6cde79550a2e99f9b8bfeb864caf8dbf6ad0d
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-06-16 17:48:41 +0330

    Add files via upload

---
+++ Asp_dotNet_7/SheypoorChi/SheypoorChi.DataLayer/Context/DatabaseContext.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SheypoorChi.DataLayer.Models;
+
+//using SheypoorChi.Core.Classes;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SheypoorChi.DataLayer.Context;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Factor> Factors { get; set; }
+    public DbSet<FactorDetail> FactorDetails { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // base.OnConfiguring(optionsBuilder);
+        options.UseSqlServer(@"Data Source=.\SQLEXPRESS; 
+                               Initial Catalog=SheypoorChiDb1;
+                               Trusted_Connection=SSPI;
+                               Encrypt=false;
+                               TrustServerCertificate=True");
+    }
+
+    protected override async void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+
+        //add first role (admin)
+        var adminRole = new Role()
+        {
+            Id = Guid.NewGuid(),
+            RoleName = "admin",
+            RoleTitle = "مدیر",
+        };
+        modelBuilder.Entity<Role>().HasData(adminRole);
+
+        //var userRole = new Role()
+        //{
+        //    Id = Guid.NewGuid(),
+        //    RoleName = "user",
+        //    RoleTitle = "کاربر",
+        //};
+        //modelBuilder.Entity<Role>().HasData(userRole);
+
+        //add first user
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            RoleId = adminRole.Id,
+            UserName = "09112223344",
+            UserPassword = await GetHash("123456789")
+        };
+        modelBuilder.Entity<User>().HasData(user);
+
+
+        //add default group list
+        //var groups = new List<Group>()
+        //{
+        //    new Group() { Id = 1, GroupName = "لوازم خانگی" },
+        //    new Group() { Id = 2, GroupName = "خودرو" },
+        //    new Group() { Id = 3, GroupName = "بازی و سرگرمی" }
+        //};
+
+        //modelBuilder.Entity<Group>().HasData(groups);
+    }
+
+    public async Task<string> GetHash(string str)
+    {
+        MD5 md5 = MD5.Create();
+        byte[] baseInput = ASCIIEncoding.Default.GetBytes(str);
+        byte[] hashInput = md5.ComputeHash(baseInput);
+
+        var hashStr = Convert.ToBase64String(hashInput);
+
+        return await Task.FromResult(hashStr);
+    }
+}
---
+++ Asp_dotNet_7/SheypoorChi/SheypoorChi.DataLayer/Models/FactorDetail.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SheypoorChi.DataLayer.Models;
+
+public class FactorDetail
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int FactorId { get; set; }
+
+    //[ForeignKey(nameof(ProductId))] //(detail) 1 : 1 (product)
+    public int ProductId { get; set; }
+
+    [Display(Name ="تعداد")]
+    public int DetailCount { get; set; }
+
+    [Display(Name ="قیمت نهایی")]
+    public int DetailPrice { get; set; }
+
+
+    [ForeignKey(nameof(FactorId))]
+    public virtual Factor? Factor { get; set; }
+
+    [ForeignKey(nameof(ProductId))]
+    public virtual Product? Product { get; set; }
+}
