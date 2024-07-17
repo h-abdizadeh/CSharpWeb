2c20a4e83fb69d28d4978210c2b3f24d2289d74a
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-07-17 09:59:53 +0330

    Update PaymentController.cs

--- PaymentController.cs
+++ PaymentController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SheypoorChi.Core.Interface;
 using SheypoorChi.Core.ViewModels;
 using System.Text;
 
@@ -25,14 +26,23 @@ public class PaymentController : Controller
     //private const string merchantId = "zarinpal-user-merchant-id";
     private const string merchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 
-    public async Task<IActionResult> RequestPaymentGate()
+    IShopping _shopping;
+    public PaymentController(IShopping shopping)
     {
+        _shopping = shopping;
+    }
+
+    public async Task<IActionResult> RequestPaymentGate(int id)//factorId
+    {
+        var factor = await _shopping.GetFactor(id);
+        if (factor is null) return RedirectToAction("index", "home");
+
         //create request data
         var requestData = new Dictionary<string, string>()
         {
             {"MerchantID",merchantId },
-            {"Amount","10000" },
-            {"CallbackURL",mainUrl},
+            {"Amount",factor.TotalPrice.ToString() },
+            {"CallbackURL",$"{mainUrl}payment/VerifyPaymentGate?factorId={factor.Id}"},
             {"Mobile","" },
             {"Description","تست درگاه پرداخت زرین پال" }
         };
@@ -57,5 +67,52 @@ public class PaymentController : Controller
         }
 
         return View();
+    }
+
+
+    public async Task<IActionResult> VerifyPaymentGate(int factorId, string status, string authority)
+    {
+        if (status.ToLower() is "ok")
+        {
+            var factor = await _shopping.GetFactor(factorId);
+            if (factor is null) return View("ShoppingCart", "home");
+
+            var verifyData = new Dictionary<string, string>()
+            {
+                {"MerchantID",merchantId},
+                {"Authority",authority},
+                {"Amount",factor.TotalPrice.ToString()},
+            };
+
+            var verifyJson = JsonConvert.SerializeObject(verifyData);
+            var verify =
+                new StringContent(verifyJson, Encoding.UTF8, "application/json");
+
+            var verifyResponse = await client.PostAsync(verifyUrl, verify);
+            var response =await verifyResponse.Content.ReadAsStringAsync();
+
+            ZarinPalVerify result = 
+                JsonConvert.DeserializeObject<ZarinPalVerify>(response);
+
+            if (result.Status is 100)
+            {
+                var id = 
+                    await _shopping.SetFactor(factor.Id,refId:result.RefID,isPay:true);
+
+                return RedirectToAction("PaymentResult", new { factorId = id });
+            }
+
+            return View();
+        }
+
+        return View();
+    }
+
+
+    public async Task<IActionResult> PaymentResult(int factorId)
+    {
+        var factor=await _shopping.GetFactor(factorId);
+
+        return View(factor);
     }
 }
