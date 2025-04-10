68407dc5d7d2a6f7e76eb0694656f51e6962c034
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2025-04-10 17:29:35 +0330

    Add files via upload

---
+++ AspNetCore6/fardaOnlineShop/wwwroot/js/MainScript.js
@@ -0,0 +1,11 @@
+﻿function GetImg(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            let img = document.getElementById('img-view')
+            img.setAttribute('src', e.target.result)
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
